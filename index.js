ec0e0256e4ea592a3969fe4c8573fd66418fcc8b
masindes <maasinde.sylvester@gmail.com>
2024-09-26 11:05:38 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+// saturDayFun function declaration
+function saturdayFun(activity = 'roller-skate') {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+// mondayWork function expression
+const mondayWork = (activity = 'go to the office') => {
+  return `This Monday, I will ${activity}.`;
+};
+
+// wrapAdjective function according to the specification
+function wrapAdjective(highlight) {
+  return function(adjective) {
+    return `You are ${highlight}${adjective}${highlight}!`;
+  };
+}
